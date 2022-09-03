ef45a1c801eba25de6d6fd0d93c13782228a13dc
Kevin Finkelmeyer <kevin.finkelmeyer@smail.inf.h-brs.de>
2022-09-04 01:54:13 +0200

    Formatting

--- src/main/kotlin/State.kt
+++ src/main/kotlin/State.kt
@@ -1,6 +1,6 @@
 package machine
 
-enum class State(val prompt:String) {
+enum class State(val prompt: String) {
     MAIN_MENU("Write action (buy, fill, take, remaining, exit): "),
     BUY("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: "),
     FILL_WATER("\nWrite how many ml of water do you want to add: "),
